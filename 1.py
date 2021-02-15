1d9c8d09a18fca79f4ec92c9259800b859382b9f
Kirill20704 <b.kirill.a.2001@gmail.com>
2021-02-15 13:18:11 +0700

    1

--- 1.py
+++ 1.py
@@ -8,4 +8,8 @@ def realcount(a, s):
 pass
 s = ['а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я', 'a', 'e', 'i', 'o', 'u', 'y']
 print(realcount(str1, s))
+str1 = TextBlob(str1)
+str1 = str1.translate()
+print(str1.sentiment.polarity)
+print(1 - str1.sentiment.subjectivity)
 
