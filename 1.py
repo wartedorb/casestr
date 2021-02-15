323750f83d0cf9585c009dc4ac6af883eeb514fe
Kirill20704 <b.kirill.a.2001@gmail.com>
2021-02-15 10:27:52 +0700

    1

--- 1.py
+++ 1.py
@@ -1,1 +1,9 @@
 str = input()
+def realcount(a, s):
+    n = 0
+    for i in range(len(s)):
+        n += a.count(s[i])
+    return(n)
+pass
+s = ['а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я']
+print(realcount(str, s))
